f15f2d8f481086c0a762fc7bbd0ba7a13d05fc30
Anuar Shaidenov <anuarshaidenov@gmail.com>
2021-08-16 23:19:08 +0600

    fixed movement date display

--- script.js
+++ script.js
@@ -11,7 +11,9 @@
 
 const account1 = {
   owner: 'Jonas Schmedtmann',
-  movements: [200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300],
+  movements: [
+    200, 455.23, -306.5, 25000, -642.21, -133.9, 79.97, 1300, 2000, -10,
+  ],
   interestRate: 1.2, // %
   pin: 1111,
 
@@ -22,8 +24,10 @@ const account1 = {
     '2020-04-01T10:17:24.185Z',
     '2020-05-08T14:11:59.604Z',
     '2020-05-27T17:01:17.194Z',
-    '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
+    '2021-08-13T23:36:17.929Z',
+    '2021-08-14T22:51:36.790Z',
+    '2021-08-15T10:22:33.990z',
+    '2021-08-16T22:22:33.990z',
   ],
   currency: 'EUR',
   locale: 'pt-PT', // de-DE
@@ -43,7 +47,7 @@ const account2 = {
     '2020-02-05T16:33:06.386Z',
     '2020-04-10T14:43:26.374Z',
     '2020-06-25T18:49:59.371Z',
-    '2020-07-26T12:01:20.894Z',
+    '2021-08-10T12:01:20.894Z',
   ],
   currency: 'USD',
   locale: 'en-US',
@@ -84,6 +88,22 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 /////////////////////////////////////////////////
 // Functions
+const formatDate = date => {
+  const now = new Date();
+  const calcDaysPassed = Math.round(
+    Math.abs(now - date) / (1000 * 60 * 60 * 24)
+  );
+  if (calcDaysPassed === 0) return `today`;
+  if (calcDaysPassed === 1) return `yesterday`;
+  if (calcDaysPassed <= 7) return `${calcDaysPassed} days ago`;
+  else {
+    const day = `${date.getDate()}`.padStart(2, 0);
+    const month = `${date.getMonth() + 1}`.padStart(2, 0);
+    const year = date.getFullYear();
+
+    return `${day}/${month}/${year}`;
+  }
+};
 
 const displayMovements = function (account, sort = false) {
   containerMovements.innerHTML = '';
@@ -94,10 +114,8 @@ const displayMovements = function (account, sort = false) {
 
   movs.forEach(function (mov, i) {
     const date = new Date(account.movementsDates[i]);
-    const day = `${date.getDate()}`.padStart(2, 0);
-    const month = `${date.getMonth() + 1}`.padStart(2, 0);
-    const year = date.getFullYear();
-    const displayDate = `${day}/${month}/${year}`;
+    const displayDate = formatDate(date);
+
     const type = mov > 0 ? 'deposit' : 'withdrawal';
     const html = `
     <div class="movements__row">
@@ -493,4 +511,12 @@ console.log(Date.now());
 
 future.setFullYear(2040);
 console.log(future);
+*/
+/*
+const future = new Date(2037, 10, 19, 15, 23);
+console.log(+future);
+
+const calcYearsPassed = (date1, date2) =>
+  Math.abs((date2 - date1) / (1000 * 60 * 60 * 24 * 365));
+console.log(calcYearsPassed(new Date(1998, 12, 31), new Date()));
 */
